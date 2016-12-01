9770dc9de8273aa2365a74a2d43900b7e6386073
CharlotteKell <Wagg9@vp.local>
2016-12-01 15:21:27 +0000

    Changes to code description
    Created the documentation help page with examples and added in how to
    use the code that will appear if the user hovers over the block with the
    mouse.

--- main.ts
+++ main.ts
@@ -5,8 +5,9 @@
 //% color=#E01818 weight=100
 namespace timer {
     /**
-     * Tooltip!
+     * Set a timer for an inputted time in minutes and seconds
      * @param mins the number of minutes to countdown, eg:1, 2, 5
+	 * @param secs the number of seconds to countdown eg: 10, 20, 30
      */
 	//% blockId=timer_countdown
 	//% block="countdown|mins%mins|secs%secs"
